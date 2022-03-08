0759353964b815fdae5857d3bf3fd1e89220514c
Alfre <fred.morieri@gmail.com>
2022-03-08 22:30:00 +0000

    Add basic structure to the game. Defined the grid function, add welcome message and essential instrcutons

--- run.py
+++ run.py
@@ -1,3 +1,38 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def print_tic_tac_toe(values):
+    """
+    Function to print Tic Tac Toe
+    """
+    print("\n")
+    print("\t     |     |")
+    print(f"\t  {values[0]}  |  {values[1]}  |  \
+{values[2]}")
+    print('\t_____|_____|_____')
+
+    print("\t     |     |")
+    print(f"\t  {values[3]}  |  {values[4]}  |  \
+{values[5]}")
+    print('\t_____|_____|_____')
+
+    print("\t     |     |")
+
+    print(f"\t  {values[6]}  |  {values[7]}  |  \
+{values[8]}")
+    print("\t     |     |")
+    print("\n")
+
+
+def main():
+    """
+    Core function of the game
+    """
+    print("\nWelcome to Tic Tac Toe. The numbers \
+in the grid indicate the cell that the player \
+will enter their symbol into")
+    grid_values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print_tic_tac_toe(grid_values)
+    print("Please enter your preferences below \
+before starting the game.")
+    # to be implemented: 1v1 and 1vPC choice
+
+
+main()
